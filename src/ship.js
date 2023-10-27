745ec72621e4824d982c3946cd980588ea453b2e
lewisthagichu <lewisthagichu254@gmail.com>
2023-10-27 09:12:55 +0300

    build: Add createShips factory function

--- src/ship.js
+++ src/ship.js
@@ -1,10 +1,44 @@
-const ship = (name, length, block) => {
-  const hit = (block) => {};
+const createShip = (shipName, shipLength) => {
+  let name = shipName;
+  let length = shipLength;
+  let timesHit = 0;
+  let isSunk = false;
+  let isFound = false;
+
+  const getName = () => name;
+  const getLength = () => length;
+  const getFound = () => isFound;
+  const getTimesHit = () => timesHit;
+  const getSunk = () => isSunk;
+
+  const found = () => {
+    isFound = true;
+  };
+
+  const hit = () => {
+    timesHit += 1;
+    if (timesHit === length) sunk();
+  };
+
+  const sunk = () => {
+    isSunk = true;
+  };
+
+  const resetFound = () => {
+    isFound = false;
+  };
 
   return {
-    name: name,
-    length: length,
+    getName,
+    getLength,
+    getFound,
+    getTimesHit,
+    getSunk,
+    hit,
+    sunk,
+    found,
+    resetFound,
   };
 };
 
-export default ship;
+export default createShip;
